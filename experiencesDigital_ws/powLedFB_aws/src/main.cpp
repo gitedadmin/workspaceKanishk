dcfec7a7d3012dac53ac9405c8cef421bb0ca84c
gitedadmin <119922992+gitedadmin@users.noreply.github.com>
2022-12-15 14:43:17 +0530

    Update main.cpp

--- experiencesDigital_ws/powLedFB_aws/src/main.cpp
+++ experiencesDigital_ws/powLedFB_aws/src/main.cpp
@@ -4,9 +4,13 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include "WiFi.h"
+#include <BH1750.h>
+#include <Wire.h>
 
 #define AWS_IOT_PUBLISH_TOPIC "esp32/pub"
 #define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
+
+BH1750 lightMeter;
 
 // main power supply pin
 const int acPin = 13;
@@ -14,26 +18,36 @@ const int acPin = 13;
 // NOTE: CANNOT USE ADC2 AS WIFI MODE IS ENABLED
 const int sensorPin = 36;
 // power status
-int powStatus;
+int powStatus, prev_powStatus;
 // light status
-int lightStatus;
+int lightStatus, prev_lightStatus;
 // initial external light intensity value
 int lightInit;
 // current light intensity value
 int lightVal;
+// connectivity
+int valRSSI;
+String intStatus;
+
+void publishMessage();
 
 WiFiClientSecure net = WiFiClientSecure();
 PubSubClient client(net);
 
 void messageHandler(char *topic, byte *payload, unsigned int length)
 {
-  Serial.print("incoming: ");
+  Serial.println("incoming: ");
   Serial.println(topic);
 
   StaticJsonDocument<200> doc;
   deserializeJson(doc, payload);
-  const char *message = doc["message"];
+  // received message
+  const int message = doc["message"];
+
+  if(message == 1)
+    publishMessage();
   Serial.println(message);
+  //publishMessage();
 }
 
 void connectAWS()
@@ -90,8 +104,9 @@ void publishMessage()
 {
   StaticJsonDocument<200> doc;
   doc["Power Status"] = powStatus;
-  //doc["Light Status"] = lightStatus;
   doc["Light Status"] = lightStatus;
+  doc["Internet Status"] = intStatus;
+  doc["Testing"] = "Hi from esp32";
   char jsonBuffer[512];
   serializeJson(doc, jsonBuffer); // print to client
 
@@ -102,10 +117,13 @@ void setup()
 {
   Serial.begin(115200);
 
+  Wire.begin();
+  lightMeter.begin();
+
   pinMode(acPin, INPUT);
 
   // reads initial intensity of external light
-  lightInit = analogRead(sensorPin);
+  lightInit = lightMeter.readLightLevel();
 
   connectAWS();
 }
@@ -113,7 +131,23 @@ void setup()
 void loop()
 {
   int pinState = digitalRead(acPin);
-  lightVal = analogRead(sensorPin);
+  lightVal = lightMeter.readLightLevel();
+
+  valRSSI = WiFi.RSSI();
+
+  // RSSI conversion table
+  
+  if(valRSSI <= -90)
+    intStatus = "Unusable";
+  else if(valRSSI >= -80 && valRSSI < -70 )
+    intStatus = "Not Good";
+  else if(valRSSI >= -70 && valRSSI < -67)
+    intStatus = "Okay";
+  else if(valRSSI >= -67 && valRSSI < -30)
+    intStatus = "Very Good";
+  else
+    intStatus = "Amazing";
+  
 
   if (pinState == HIGH)
   {
@@ -123,24 +157,35 @@ void loop()
   }
   else
   {
+    //publishMessage();
     powStatus = 0;
     // digitalWrite(logPin, HIGH);
     Serial.println("Power OFF");
   }
 
   // 300 is an experimental value 
-  if ((lightVal) < 300)
+  if ((lightVal - lightInit) < 300)
   {
     Serial.print("Billboard ON");
-    lightStatus = 1;
+    lightStatus = 0;
   }
   else
   {
+    //publishMessage();
     Serial.print("Billboard OFF");
-    lightStatus = 0;
+    lightStatus = 1;
   }
 
-  publishMessage();
+  
+  if((prev_lightStatus^lightStatus)||(prev_powStatus^powStatus))
+    publishMessage();
+  
+
+  //delay(200);
+  
+  prev_lightStatus = lightStatus;
+  prev_powStatus = powStatus;
+  //publishMessage();
   client.loop();
   delay(1000);
 }
